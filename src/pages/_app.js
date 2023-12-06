60c9a61d382b86e9bfe68353c0e61c0a18321b0b
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2023-12-06 12:12:54 +0530

    site icon set

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,25 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import Head from "next/head";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="top-0 left-0 right-0">
-        <Navbar />
+    <>
+      <Head>
+        <link rel="icon" href="/logo.png" />
+      </Head>
+
+      <div className="flex flex-col min-h-screen">
+        <div className="top-0 left-0 right-0">
+          <Navbar />
+        </div>
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
       </div>
-      <main className="flex-grow">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
+    </>
   );
 }
