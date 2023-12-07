0cc617e00bf426c675001566b2990a64b7a4a216
Priya Tiwari <87354985+08pt@users.noreply.github.com>
2023-12-07 15:27:45 +0530

    og graph added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,7 +18,27 @@ export default function App({ Component, pageProps }) {
           name="keywords"
           content="Event Management Company in Delhi, Wedding Planner in Delhi, Best Wedding Planner in Delhi, Wedding Planner in India, Wedding Decorator in Delhi, AV Sound Setup in Delhi, Event Production Company in Delhi, Event planning services, Event management company, Professional event planners, Corporate event planning, Wedding planning services, Party planners, Special event coordination, Event design and production, Event logistics and coordination, Venue selection and management, Event decoration and styling, Budget-friendly event planning, Event marketing and promotion, Event entertainment services, Destination event planning, Event registration and ticketing, Event branding and sponsorship, Social media marketing for events, Event technology solutions, Event planning checklist"
         />
+        {/* Add Open Graph meta tags */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Bracelete Production | Event Management Company in India"
+        />
+        <meta
+          property="og:description"
+          content="Discover seamless event planning and execution with our expert team. From corporate galas to intimate gatherings, we specialize in crafting unforgettable experiences"
+        />
+        <meta
+          property="og:image"
+          content="https://www.braceletproductions.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fwedding_cover.b22f0707.jpg&w=1200&q=75"
+        />
+        {/* Replace with the actual image URL */}
+        <meta property="og:url" content="https://www.braceletproductions.in/" />
+        <meta property="og:site_name" content="Bracelete Productions" />
+        {/* site icon  */}
+
         <link rel="icon" href="/logo.png" />
+        {/* canonical url common link  */}
         <link rel="canonical" href="https://www.braceletproductions.in/" />
       </Head>
 
